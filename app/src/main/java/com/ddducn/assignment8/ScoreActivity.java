ed82b0dc19cf32f58913ef80cb0d8865d834b8ef
Ander <531491265@qq.com>
2020-06-14 19:32:26 +0800

    Task 25: Define icons & color for score screen & Task 26: Layout the score screen

--- app/src/main/java/com/ddducn/assignment8/ScoreActivity.java
+++ app/src/main/java/com/ddducn/assignment8/ScoreActivity.java
@@ -1,12 +1,17 @@
 package com.ddducn.assignment8;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
 
 public class ScoreActivity extends FullScreenActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+
     }
 }
